eb2a29564c67e97ce2cd1f79a7aa3e8ddaaea791
Gerson Funez <gersonfunez43@gmail.com>
2021-05-02 00:11:39 -0600

    3

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('./dist/fronted'));
+app.use(express.static('./dist/frontend'));
 
 app.get('/*', function (req, res) {
   res.sendFile('index.html', {
-    root: 'dist/fronted'
+    root: 'dist/frontend'
   });
 });
 
